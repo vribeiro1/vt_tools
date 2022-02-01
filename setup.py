3706ecf6f188490e9a640a24a2a1115e81447d35
Vinicius Ribeiro <vinicius.souza-ribeiro@loria.fr>
2022-02-01 14:51:11 +0100

    Basic project structure

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+requirements = [
+    "funcy",
+    "matplotlib",
+    "numpy==1.21",
+    "torch",
+    "ujson"
+]
+
+setup(
+    name="vt_tools",
+    version="0.0.1",
+    description="Vocal tract tools",
+    author="Vinicius Ribeiro",
+    author_email="vinicius.souza-ribeiro@loria.fr",
+    license="MIT",
+    packages=find_packages(),
+    zip_safe=False,
+    install_requirements=requirements
+)
---
+++ vt_tools/__init__.py
@@ -0,0 +1,25 @@
+ARYTENOID_MUSCLE = "arytenoid-muscle"
+EPIGLOTTIS = "epiglottis"
+LOWER_INCISOR = "lower-incisor"
+LOWER_LIP = "lower-lip"
+PHARYNX = "pharynx"
+SOFT_PALATE = "soft-palate"
+THYROID_CARTILAGE = "thyroid-cartilage"
+TONGUE = "tongue"
+UPPER_INCISOR = "upper-incisor"
+UPPER_LIP = "upper-lip"
+VOCAL_FOLDS = "vocal-folds"
+
+COLORS = {
+    ARYTENOID_MUSCLE: "blueviolet",
+    EPIGLOTTIS: "turquoise",
+    LOWER_INCISOR: "cyan",
+    LOWER_LIP: "lime",
+    PHARYNX: "goldenrod",
+    SOFT_PALATE: "dodgerblue",
+    THYROID_CARTILAGE: "saddlebrown",
+    TONGUE: "darkorange",
+    UPPER_INCISOR: "yellow",
+    UPPER_LIP: "magenta",
+    VOCAL_FOLDS: "hotpink"
+}
