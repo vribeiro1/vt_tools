7bf4f38516151ab597cc3b383f09e4f45e356020
Vinicius Ribeiro <vinicius.souza-ribeiro@loria.fr>
2023-02-21 14:37:21 +0100

    Add p2cp RMS metric

--- vt_tools/metrics.py
+++ vt_tools/metrics.py
@@ -9,8 +9,8 @@ def euclidean(u, v):
     Calculates the euclidean distance between two points. Use numba to accelerate the calculation.
 
     Args:
-    u (Tuple[float, float]): First point to calculate the euclidean distance.
-    v (Tuple[float, float]): Second point to calculate the euclidean distance.
+        u (Tuple[float, float]): First point to calculate the euclidean distance.
+        v (Tuple[float, float]): Second point to calculate the euclidean distance.
 
     >>> u = np.array([3., 0.])
     >>> v = np.array([0., 4.])
@@ -29,9 +29,9 @@ def p2cp(i, u, v):
     Calculates the point to closest point distance between the u[i] point to the v curve.
 
     Args:
-    i (int): Index of the point in the u-array.
-    u (np.ndarray): Array of shape (N, 2) that contains the target point.
-    v (np.ndarray): Array of shape (N, 2) to calculate the closest point.
+        i (int): Index of the point in the u-array.
+        u (np.ndarray): Array of shape (N, 2) that contains the target point.
+        v (np.ndarray): Array of shape (N, 2) to calculate the closest point.
     """
     ui = u[i]
     ui2cp = min(euclidean(ui, vj) for vj in v)
@@ -44,8 +44,8 @@ def distance_matrix(u, v):
     Calculates the distance between the elements of two arrays.
 
     Args:
-    u (np.ndarray): Array of shape (N, 2).
-    v (np.ndarray): Array of shape (N, 2).
+        u (np.ndarray): Array of shape (N, 2).
+        v (np.ndarray): Array of shape (N, 2).
     """
     n = len(u)
     m = len(v)
@@ -63,8 +63,8 @@ def p2cp_mean(u_, v_):
     Calculates the mean point-to-closest-point distance between two arrays.
 
     Args:
-    u_ (np.ndarray): Array of shape (N, 2).
-    v_ (np.ndarray): Array of shape (N, 2).
+        u_ (np.ndarray): Array of shape (N, 2).
+        v_ (np.ndarray): Array of shape (N, 2).
     """
     n = len(u_)
     m = len(v_)
@@ -76,3 +76,23 @@ def p2cp_mean(u_, v_):
     mean_p2cp = ((np.sum(u2cv) / n) + (np.sum(v2cu) / m)) / 2
 
     return mean_p2cp
+
+def p2cp_rms(u_, v_):
+    """
+    Calculates the RMS value of the point-to-closest-point distance between two arrays.
+
+    Args:
+        u_ (np.ndarray): Array of shape (N, 2).
+        v_ (np.ndarray): Array of shape (N, 2).
+    """
+    n = len(u_)
+    m = len(v_)
+    assert n == m, "Arrays have to have the same length for the RMS value."
+
+    dist_mtx = distance_matrix(u_, v_)
+    u2cv = dist_mtx.min(axis=1)
+    v2cu = dist_mtx.min(axis=0)
+    p2cp = (u2cv + v2cu) / 2
+    p2cp_rms = (p2cp ** 2).sum() / n
+
+    return p2cp_rms
